6aefd62765256f6590c05b59a3cf19816a29c3ee
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-11-10 18:23:11 -0700

    Next attempted fix for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,12 +21,10 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.assets.js_compressor = Uglifier.new(harmony: true)
-  # Compress JavaScripts and CSS.
-  # config.assets.js_compressor = :uglifier
-  # possible fix for the Heroku deployment; uncomment line 28 and attempt to push to Heroku
   # config.assets.js_compressor = Uglifier.new(harmony: true)
-  # config.assets.css_compressor = :sass
+  # Compress JavaScripts and CSS.
+  config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
