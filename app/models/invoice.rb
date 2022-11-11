74c2800794c0f7efba6dd8ebf1691fb07da14cdd
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-11-11 13:20:22 -0700

    Updates model relationships to match starter repo

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,8 +1,9 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
+  has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
-  has_many :transactions
+  has_many :merchants, through: :items
 
   enum status: [ :completed, :cancelled, "in progress" ]
 
