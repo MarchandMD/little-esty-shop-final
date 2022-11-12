dbbc84491dcde8d4fa5cec18e8ccc4357c7aedf1
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-11-11 22:42:12 -0700

    Updates models with new relationships

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,3 +1,6 @@
 class Discount < ApplicationRecord
   belongs_to :merchant
+  has_many :invoices, through: :merchant
+  has_many :transactions, through: :invoices
+  has_many :customers, through: :invoices
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,4 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
+  has_many :discounts, through: :invoice
 end
