32f9002908f70474601b8605263973333b5066c8
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-11-15 22:53:52 -0700

    Applies API call and display

---
+++ app/poros/holidays.rb
@@ -0,0 +1,8 @@
+require 'httparty'
+require 'pry'
+
+response = HTTParty.get("https://date.nager.at/api/v3/NextPublicHolidays/US")
+
+parsed = JSON.parse(response.body, symbolize_names: true)
+
+require 'pry'; binding.pry
