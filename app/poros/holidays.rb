550dd27c4d957e65d53aae84522456c77ee69d66
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-11-15 23:52:32 -0700

    Updates views for visual aesthetics and removes pry from dummy file

--- app/poros/holidays.rb
+++ app/poros/holidays.rb
@@ -1,8 +1,7 @@
 require 'httparty'
-require 'pry'
+
 
 response = HTTParty.get("https://date.nager.at/api/v3/NextPublicHolidays/US")
 
 parsed = JSON.parse(response.body, symbolize_names: true)
 
-require 'pry'; binding.pry
