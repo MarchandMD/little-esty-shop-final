c698d966aa92fa24238c604f47ead9a365e5cce0
Michael Marchand <michaeldavidmarchand@gmail.com>
2023-05-26 08:14:09 -0600

    moves welcome text to welcome.md

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,34 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @hello_world = "# Little Esty Shop
-
-## Background and Description
-
-'Little Esty Shop' is a group project that requires students to build a fictitious e-commerce platform where merchants and admins can manage inventory and fulfill customer invoices.
-
-## Learning Goals
-- Practice designing a normalized database schema and defining model relationships
-- Utilize advanced routing techniques including namespacing to organize and group like functionality together.
-- Utilize advanced active record techniques to perform complex database queries
-- Practice consuming a public API while utilizing POROs as a way to apply OOP principles to organize code
-
-## Requirements
-- must use Rails 5.2.x
-- must use PostgreSQL
-- all code must be tested via feature tests and model tests, respectively
-- must use GitHub branching, team code reviews via GitHub PR comments, and either GitHub Projects or a project management tool of your group's choice (Trello, Notion, etc.)
-- must include a thorough README to describe the project
-   - README should include a basic description of the project, a summary of the work completed, and some ideas for a potential contributor to work on/refactor next. Also include the names and GitHub links of all student contributors on your project.
-- must deploy completed code to Heroku
-- Continuous Integration / Continuous Deployment is not allowed
-- Use of scaffolding is not allowed
-- Any gems added to the project must be approved by an instructor
-
-## Phases
-
-1. [Database Setup](https://github.com/turingschool-examples/little-esty-shop/blob/main/doc/db_setup.md)
-1. [User Stories](https://github.com/turingschool-examples/little-esty-shop/blob/main/doc/user_stories.md)
-1. [Extensions](https://github.com/turingschool-examples/little-esty-shop/blob/main/doc/extensions.md)
-1. [Evaluation](https://github.com/turingschool-examples/little-esty-shop/blob/main/doc/evaluation.md)"
+    @hello_world = File.read('welcome.md')
   end
 end
